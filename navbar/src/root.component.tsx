bf549e2b3736ae0c757ec9beb00b8f3a07227bdb
Robert Kibet <kipronokrobert@gmail.com>
2021-08-25 09:59:29 +0300

    cleanup cache and comit afresh

---
+++ navbar/src/root.component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Root() {
+  return (
+    <nav className="nav">
+      <a href="/page1" className="link">
+        Page 1
+      </a>
+      <a href="/page2" className="link">
+        Page 2
+      </a>
+    </nav>
+  );
+}
---
+++ root-config/src/buerto-root-config.ts
@@ -0,0 +1,29 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const data = {
+  loaders: {
+    defaultLoader: `
+    <div class="lds-ripple"><div></div><div></div></div>
+    `,
+  },
+  props: {},
+};
+
+const routes = constructRoutes(microfrontendLayout, data);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
